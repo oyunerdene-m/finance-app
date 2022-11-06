280f88e04674bc6ce85adad07e741c03630c0f22
Oyun-Erdene Munkhbaatar <oyunerdene.m@gmail.com>
2022-11-06 11:09:11 +0100

    rename front-end => front

--- front-end/src/App.js
+++
@@ -1,7 +0,0 @@
-import './App.css';
-
-function App() {
-	return <div className="App">Learn react</div>;
-}
-
-export default App;
---
+++ front/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Login from './components/User/Login';
+import CreateUser from './components/User/CreateUser';
+
+function App() {
+	return (
+		<div className="App">
+			Learn react
+			<Login />
+			<br />
+			<CreateUser />
+		</div>
+	);
+}
+
+export default App;
