27f30838cdf6f24ef18b5b5b5a677204c8950d74
Oyun-Erdene Munkhbaatar <oyunerdene.m@gmail.com>
2022-11-07 18:09:25 +0100

    changed accounts properties, created create form and changed jsx in Account

--- front/src/components/Accounts/Account.jsx
+++ front/src/components/Accounts/Account.jsx
@@ -1,12 +1,12 @@
 import classes from './Account.module.css';
 
 export default function Account(props){
-    const {name, type, initialBalance, currency, bankName} = props.account
+    const {name, type, amount, currency, description} = props.account
  return ( 
     <li className={classes.account}>
-        <h4>{name}</h4>
-        <p>balance: {initialBalance} {currency}</p>
-        <p>{bankName} - {type}</p>
+        <h4>{name} - {type}</h4>
+        <p>balance: {currency}{amount} </p>
+        <p>{description}</p>
     </li>
  )
 }
--- front/src/components/Accounts/CreateAccount.jsx
+++ front/src/components/Accounts/CreateAccount.jsx
@@ -1,5 +1,30 @@
 export default function CreateAccount(){
     return (
-        <form></form>
+        <form>
+            <label htmlFor="name">Account name:</label>
+            <input type="text" id="name" name="name"/>
+            <br/>
+            <label htmlFor="amount">Amount</label>
+            <input type="number" id="amount" name="amount"/>
+            <br/>
+            <label htmlFor="description">Description</label>
+            <input type="text" id="description" name="description"/>
+            <br/>
+            <label htmlFor="type">Type</label>
+            <select name="type" id="type">
+                <option value="cash">Cash</option>
+                <option value="card">Card</option>
+                <option value="savings">Savings</option>
+                <option value="loan">Loan</option>
+                <option value="others">Others</option>
+            </select>
+            <br/>
+            <label htmlFor="currency">Currency</label>
+            <select name="currency" id="currency">
+                <option value="euro">Euro</option>
+                <option value="dollar">Dollar</option>
+                <option value="tugrug">Tugrug</option>
+            </select>
+        </form>
     )
 }
