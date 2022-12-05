5219b5b661052408385cfa1c7565a2ad467f5b1e
Oyun-Erdene Munkhbaatar <oyunerdene.m@gmail.com>
2022-12-05 18:35:20 +0100

    added styles

--- front/src/pages/transactions.jsx
+++ front/src/pages/transactions.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import TransactionList from '../components/Transactions/Transactions/TransactionList';
 import fetchData from '../lib/fetchData';
+import AddIcon from '../components/UI/AddIcon';
 
 export default function Transactions() {
 	const [transactions, setTransactions] = useState([]);
@@ -25,16 +26,7 @@ export default function Transactions() {
 				<h3>Last transactions</h3>
 				<Link to='/transactions/new'>
 					{' '}
-					<svg
-						xmlns='http://www.w3.org/2000/svg'
-						fill='none'
-						viewBox='0 0 24 24'
-						strokeWidth={1.5}
-						stroke='currentColor'
-						className='w-6 h-6 border border-more-gray rounded p-0.5'
-					>
-						<path strokeLinecap='round' strokeLinejoin='round' d='M12 4.5v15m7.5-7.5h-15' />
-					</svg>
+					<AddIcon />
 				</Link>
 			</div>
 			<TransactionList transactions={transactions} />
