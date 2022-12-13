6d59bb589dd33a60eb0a9799cb40c4e2d06a375c
Oyun-Erdene Munkhbaatar <oyunerdene.m@gmail.com>
2022-12-13 14:18:37 +0100

    modal width fix

--- front/src/components/UI/Modal.jsx
+++ front/src/components/UI/Modal.jsx
@@ -5,7 +5,7 @@ export default function Modal(props) {
 				className='fixed top-0 left-0 w-full h-screen z-1 backdrop-contrast-75'
 				onClick={props.onCancel}
 			/>
-			<div className='absolute w-1/5 z-100 overflow-hidden bg-light-blue px-4 py-5 text-light-gray rounded-md'>
+			<div className='absolute w-3/5 lg:w-2/5 xl:w-1/5 z-100 overflow-hidden bg-light-blue px-4 py-5 text-light-gray rounded-md'>
 				{props.children}
 			</div>
 		</>
